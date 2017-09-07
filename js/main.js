676e9ff84dc43e4f00bbd5d0fba29cf8e0fd7318
flipggs <flipggs@gmail.com>
2017-09-07 12:10:10 -0300

    Add IIFE - Funcao auto-execuvel

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-'use strict'
+(function(){
+    console.log("Funcao IIFE - Funcao auto-executavel")
+    console.log("Deve ser executado no inicio do arquivo")
+}());
 
 function is(obj) {
     return Object.prototype.toString.call(obj);
