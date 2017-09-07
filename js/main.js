bba47d4e5945747a900cfb195713e723df86aeb7
flipggs <flipggs@gmail.com>
2017-09-06 23:10:45 -0300

    Add methods validation Types JS

---
+++ js/main.js
@@ -0,0 +1,31 @@
+'use strict'
+
+function is(obj) {
+    return Object.prototype.toString.call(obj);
+}
+
+function isNumber(obj) {
+    return is(obj) == "[object Number]";
+}
+
+function isString(obj) {
+    return is(obj) == "[object String]";
+}
+
+function isArray(obj) {
+    return is(obj) == "[object Array]";
+}
+
+function isObject(obj) {
+    return is(obj) == "[object Object]";
+}
+
+function isBoolen(obj) {
+    return is(obj) == "[object Boolean]";
+}
+
+function isFunc(obj) {
+    return is(obj) == "[object Function]";
+}
+
+console.log(isFunc("function(){}"))
