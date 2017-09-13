e8ba7c7dce9e87e6a4d653c87c583c1994b29e13
flipggs <flipggs@gmail.com>
2017-09-12 22:10:55 -0300

    Arrow Function Sintaxe Block

--- js/arrowFunction.js
+++ js/arrowFunction.js
@@ -13,3 +13,12 @@ console.log(frutas.map(function(fruta){
 
 console.log("console.log(frutas.map(fruta => fruta.toUpperCase()))");
 console.log(frutas.map(fruta => fruta.toUpperCase()))
+
+
+console.log('Arrow Function Sintaxe Block');
+const nomes = ['Felipe', 'Siqueira', 'Dev'].map( nome => {
+    nome = nome.toUpperCase();
+    return `${nome} tem ${nome.length} caracteres`;
+});
+
+console.log(nomes);
