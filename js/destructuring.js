c18eb4d08d817dbca418326904ec912da1f33688
flipggs <flipggs@gmail.com>
2017-09-14 23:01:56 -0300

    Add Func params Default

--- js/destructuring.js
+++ js/destructuring.js
@@ -5,3 +5,7 @@ const {name, age} = person
 console.log('destructuring');
 console.log(name) // 'Matheus'
 console.log(age) // 26
+
+function funcParamsDefault(pram1 = 'teste', pram2 = 'teste2') {
+    console.log(pram1 + ' - ' + pram2);
+}
